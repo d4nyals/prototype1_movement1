4f3c58646eb95b33f8853f223737ad5837cd09dc
dann <93323948+d4nyals@users.noreply.github.com>
2025-10-02 14:48:24 +0100

    Update prototype1.py

--- prototype1.py
+++ prototype1.py
@@ -1,1 +1,11 @@
+import pygame # Code will be coded in Pygame. Code.
+pygame.init()
+display = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Simple PyGame Example')
+clock = pygame.time.Clock()
+running = True
+
+# --- constants ---
+WIDTH = 1280  # 16:9 aspect ratio width
+HEIGHT = 720  # 16:9 aspect ratio height , perfect for PC displays
 
